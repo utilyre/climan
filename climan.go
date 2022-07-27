2c1c9958a5fc8f6f6090aac7222e4d77716036bc
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-27 15:58:56 +0430

    feat(cli): Accept a http file as the first argument

--- climan.go
+++ climan.go
@@ -4,19 +4,28 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	nth := flag.Int("nth", -1, "only run the nth request in the file")
 	flag.Parse()
 
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	filename := flag.Arg(0)
+
 	if ordinalized, err := ordinalize(*nth); err == nil {
-		fmt.Printf("Running the %s request...\n", ordinalized)
+		fmt.Printf("Running the %s request of %s...\n\n", ordinalized, filename)
+
 		fmt.Println("{\"message\":\"Hello world!\"}")
 		return
 	}
 
-	fmt.Println("Running all requests...")
+	fmt.Printf("Running all requests of %s...\n", filename)
 }
 
 func ordinalize(num int) (string, error) {
