b0947ed350f3a8b7963e4412329dc63f74a9933d
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-27 16:03:19 +0430

    feat(cli): Improve ordinalize function

--- climan.go
+++ climan.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -29,17 +30,18 @@ func main() {
 }
 
 func ordinalize(num int) (string, error) {
-	if num < 0 {
+	if num <= 0 {
 		return "", errors.New("natural numbers can only be ordinalized")
 	}
 
-	switch num {
-	case 1:
-		return "1st", nil
-	case 2:
-		return "2nd", nil
-	case 3:
-		return "3rd", nil
+	strNum := strconv.Itoa(num)
+	switch strNum[len(strNum)-1:] {
+	case "1":
+		return fmt.Sprintf("%dst", num), nil
+	case "2":
+		return fmt.Sprintf("%dnd", num), nil
+	case "3":
+		return fmt.Sprintf("%drd", num), nil
 	}
 
 	return fmt.Sprintf("%dth", num), nil
