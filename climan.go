0371cc76ce4776763cde1c2c3fb8d239c180d7a4
Amirabbas Fazelinia <utilyre@proton.me>
2022-07-27 15:50:34 +0430

    feat(proto): implement how user would interact with program

--- climan.go
+++ climan.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	nth := flag.Int("nth", -1, "only run the nth request in the file")
+	flag.Parse()
+
+	if ordinalized, err := ordinalize(*nth); err == nil {
+		fmt.Printf("Running the %s request...\n", ordinalized)
+		fmt.Println("{\"message\":\"Hello world!\"}")
+		return
+	}
+
+	fmt.Println("Running all requests...")
+}
+
+func ordinalize(num int) (string, error) {
+	if num < 0 {
+		return "", errors.New("natural numbers can only be ordinalized")
+	}
+
+	switch num {
+	case 1:
+		return "1st", nil
+	case 2:
+		return "2nd", nil
+	case 3:
+		return "3rd", nil
+	}
+
+	return fmt.Sprintf("%dth", num), nil
 }
