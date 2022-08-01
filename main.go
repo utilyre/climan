b723e9c863538b378ec1d36e3ddab24bbee4142a
Amirabbas Fazelinia <utilyre@proton.me>
2022-08-01 13:24:52 +0430

    fix(error): Directly use log.Fatal instead of panic

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/utilyre/climan/request"
 )
@@ -13,43 +12,37 @@ import (
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("climan: ")
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-	}()
 
 	var nth int
 	flag.IntVar(&nth, "n", 0, "run the nth request of <filename>")
 
 	flag.Parse()
 	if nth <= 0 {
-		panic("n must be greater than 0")
+		log.Fatalln("n must be greater than 0")
 	}
 
 	filename := flag.Arg(0)
 	if filename == "" {
-		panic("missing filename")
+		log.Fatalln("missing filename")
 	}
 
 	requests, err := request.ParseHTTP(filename)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	if nth > len(requests) {
-		panic(fmt.Sprintf("n must be less than %d", len(requests)))
+		log.Fatalln(fmt.Sprintf("n must be less than %d", len(requests)))
 	}
 
 	var data any
 	err = requests[nth-1].Run(&data)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	raw, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	fmt.Println(string(raw))
