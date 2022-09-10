3e6f3eaa7e22056956109403778aab33e4fb32a8
Amirabbas Fazelinia <utilyre@proton.me>
2022-09-10 18:25:59 +0430

    CLIMAN v0.2.1

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/utilyre/climan/httpparser"
 )
 
-const version string = "0.2.0"
+const version string = "0.2.1"
 
 var (
 	showHelp    *bool   = getopt.BoolLong("help", 'h', "show help")
